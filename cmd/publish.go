c19faffa406d0b85c9871939d362edb2400ce84b
Scott Fleckenstein <nullstyle@gmail.com>
2016-04-29 14:51:13 -0700

    Improve publishing, allow merging of new data

--- cmd/publish.go
+++ cmd/publish.go
@@ -2,9 +2,13 @@ package cmd
 
 import (
 	"fmt"
+	// "log"
+	"path/filepath"
 
+	"github.com/dappstore/go-dapp"
 	"github.com/dappstore/go-dapp/protocols/dfs"
 	"github.com/dappstore/go-dapp/protocols/publish"
+	"github.com/gosuri/uilive"
 	"github.com/spf13/cobra"
 )
 
@@ -17,26 +21,42 @@ var publishCmd = &cobra.Command{
 	Long:  `TODO`,
 	Run: func(cmd *cobra.Command, args []string) {
 		login()
-		pfs := dfs.New(app.Providers)
+		writer := uilive.New()
+		writer.Start()
+
+		pdfs := dfs.New(app.Providers)
 		ppublish := publish.New(app.Providers, app.Providers)
 
-		content, err := pfs.StoreLocalPaths(args)
+		fmt.Fprintln(writer, "loading current publications hash...")
+		source, err := ppublish.GetPublications(app.CurrentUser())
 		mustSucceed(err)
 
-		tx, pubs, err := ppublish.SetPublications(app.CurrentUser(), content)
+		current := source
+
+		for _, path := range args {
+			var toAdd dapp.Hash
+			name := filepath.Base(path)
+			toAdd, err = pdfs.StorePath(path)
+			mustSucceed(err)
+			fmt.Fprintf(writer, "merging %s into %s\n", path, current)
+			current, err = pdfs.MergeAtPath(current, name, toAdd)
+			mustSucceed(err)
+		}
+
+		fmt.Fprintf(writer, "setting %s as publications...\n", current)
+		tx, pubs, err := ppublish.SetPublications(app.CurrentUser(), current)
 		mustSucceed(err)
+
+		fmt.Fprint(writer, "Success!\n\n")
+		writer.Stop()
 
 		fmt.Println("publisher:", app.CurrentUser())
-		fmt.Println("new publications hash:", pubs)
+		fmt.Println("content hash:", current)
+		fmt.Println("publications hash:", pubs)
 		fmt.Println("transaction hash:", tx)
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(publishCmd)
-	// publishCmd.Flags().StringVarP(
-	// 	&pathToPublish, "path", "",
-	// 	"",
-	// 	"path to publish FILES at",
-	// )
 }
